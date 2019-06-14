68f040add80b611d83d5ff81d816bff8b78d8c4e
Brett Onions <onionsllb@outlook.com>
2019-06-14 15:49:21 +0200

    Folder set up and storing the payload locally
    Mostly setting up the folder structure and creating a local instance of the JSON data to remove dependence on internet connection

---
+++ bin/app.js
@@ -0,0 +1,2 @@
+let fs = require('request');
+let categoryData = JSON.parse(fs.writeFileSync('./jsondata/categoryData.json'));
---
+++ bin/save-json.js
@@ -0,0 +1,23 @@
+let request = require('request');
+let fs      = require('fs');
+let path    = require('path');
+
+
+function createTestCaseFile()
+{
+    request.get('http://142.93.203.254:4200/test-case-1')
+    .on('error', (err) =>{
+        console.log(err);
+    }).pipe(
+        fs.createWriteStream(path.join(__dirname,'/jsondata/test-case-1.json'))
+    );
+
+    request.get('http://142.93.203.254:4200/test-case-2')
+    .on('error', (err) =>{
+        console.log(err);
+    }).pipe(
+        fs.createWriteStream(path.join(__dirname,'/jsondata/test-case-2.json'))
+    );
+}
+
+module.exports = createTestCaseFile;
